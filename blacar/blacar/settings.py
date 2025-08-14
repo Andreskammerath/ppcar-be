fdb56c9c133793f62df2eb0712a3c54f39a198c4
Ezequiel Bitschin <ezequielbitschin@MacBook-Pro-de-Ezequiel.local>
2025-08-14 18:07:09 -0300

    Add standarized errors to openapi schemas

--- blacar/blacar/settings.py
+++ blacar/blacar/settings.py
@@ -158,7 +158,7 @@ AUTH_USER_MODEL = 'startup.User'
 DOMAIN_EVENTS_BROKER = "startup.shared.events.DjangoSignalDomainEventBroker"
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_standardized_errors.openapi.AutoSchema',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'startup.shared.repository.PageNumberPagination',
@@ -176,6 +176,20 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'API for the blacar startup project',
     'VERSION': '0.1.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    "ENUM_NAME_OVERRIDES": {
+        "ValidationErrorEnum": "drf_standardized_errors.openapi_serializers.ValidationErrorEnum.choices",
+        "ClientErrorEnum": "drf_standardized_errors.openapi_serializers.ClientErrorEnum.choices",
+        "ServerErrorEnum": "drf_standardized_errors.openapi_serializers.ServerErrorEnum.choices",
+        "ErrorCode401Enum": "drf_standardized_errors.openapi_serializers.ErrorCode401Enum.choices",
+        "ErrorCode403Enum": "drf_standardized_errors.openapi_serializers.ErrorCode403Enum.choices",
+        "ErrorCode404Enum": "drf_standardized_errors.openapi_serializers.ErrorCode404Enum.choices",
+        "ErrorCode405Enum": "drf_standardized_errors.openapi_serializers.ErrorCode405Enum.choices",
+        "ErrorCode406Enum": "drf_standardized_errors.openapi_serializers.ErrorCode406Enum.choices",
+        "ErrorCode415Enum": "drf_standardized_errors.openapi_serializers.ErrorCode415Enum.choices",
+        "ErrorCode429Enum": "drf_standardized_errors.openapi_serializers.ErrorCode429Enum.choices",
+        "ErrorCode500Enum": "drf_standardized_errors.openapi_serializers.ErrorCode500Enum.choices",
+    },
+    "POSTPROCESSING_HOOKS": ["drf_standardized_errors.openapi_hooks.postprocess_schema_enums"]
 }
 
 DEFAULT_FROM_EMAIL = "blacar@blacar.com"
